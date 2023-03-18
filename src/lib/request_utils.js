c46c4cfff8100d67079eccc6d4533b0e4995b18c
Maxwell D. Dorliea <deboy170@gmail.com>
2023-03-18 04:58:34 +0000

    Added production api base url

--- src/lib/request_utils.js
+++ src/lib/request_utils.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://127.0.0.1:8000/api/v1';
+const BASE_URL = 'https://api.maxpay.maxwelldorliea.tech';
 
 export const getData = async (id, url) => {
     const res = await fetch(`${BASE_URL}/${url}/${id}`);
